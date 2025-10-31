c2fca3919cbbe654fc073772e834d9b3618634c5
NunoCunha43 <1211689@isep.ipp.pt>
2025-10-31 22:22:05 +0000

    #39 - Provision.sh changed - added line to run server

--- CA3/Part1/provision.sh
+++ CA3/Part1/provision.sh
@@ -24,3 +24,6 @@ xvfb-run ./gradlew build
 
 cd /vagrant/cogsi2526-1240444-1211426-1211689/CA2/Part2
 ./gradlew bootJar
+
+cd /vagrant/cogsi2526-1240444-1211426-1211689/CA2/Part1/gradle_basic_demo-main
+./gradlew runServer
